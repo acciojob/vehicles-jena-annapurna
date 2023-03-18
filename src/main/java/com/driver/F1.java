202fd904121643c03c8b06072c6be7b8ae1ffeaa
jena-annapurna <annapurnajena03@gmail.com>
2023-03-18 16:34:54 +0530

    vehicle_solution1

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,12 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+       super(name,4,4,6,isManual,name,1);
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed=0;//set the value of new speed by using currentSpeed and rate
+        newSpeed=getCurrentSpeed()+rate;
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,11 +22,45 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            super.stop();
+            setCurrentGear(1);
+
         }
         //for all other cases, change the gear accordingly
-
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+       else if(newSpeed>=1 && newSpeed<=50)
+        {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(1);
         }
+        else if(newSpeed>=51 && newSpeed<=100)
+        {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(2);
+        }
+       else if(newSpeed>=101 && newSpeed<=150)
+        {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(3);
+        }
+       else if(newSpeed>=151 && newSpeed<=200)
+        {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(4);
+        }
+      else if(newSpeed>=201 && newSpeed<=250)
+        {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(5);
+        }
+        else if(newSpeed>250)
+        {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(6);
+        }
+        if(newSpeed>0)
+        {
+            changeSpeed(newSpeed,getCurrentDirection());
+        }
+
     }
 }
